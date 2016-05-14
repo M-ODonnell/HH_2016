22223ff1e7e86f6a121cde4dc7516f27c1e1a506
Meghan O'Donnell <meghan.tara@gmail.com>
2016-05-14 16:09:34 -0400

    forum add new topic

--- js/forum.js
+++ js/forum.js
@@ -2,21 +2,25 @@
  * Created by modonnell on 5/14/16.
  */
 $(document).ready(function() {
+    var data;
     $('#newTopicForm').hide();
-    $.getJSON("./../data/forum.json", function(data) {
-        console.log("DATA ", data);
-
+    $.getJSON("./../data/forum.json", function(inData) {
+        data = inData;
+        renderForum(data);
     });
     $('#addNewTopic').click(function() {
         $('#newTopicForm').show();
         $('#addNewTopic').hide();
     });
     $('#cancelTopic').click(function() {
-        $('#newTopicForm').hide();
-        $('#addNewTopic').show();
+        hideAndClear(); 
     });
     $('#submitTopic').click(function() {
-
+        var newTopic = $('#topic').val();
+        var newMessage = $('#message').val();
+        data.posts.push({"topic": newTopic, "message": newMessage, "replies": []});
+        renderForum(data);
+        hideAndClear();
     });
 
 
@@ -24,9 +28,21 @@ $(document).ready(function() {
 });
 
 var postRowTemplate = _.template(
-    '<% _.forEach(posts, function(post) { %> }); %>'
-    <tr>
-        <td><%= topic %></td>
-        <td><%= message %></td>
-    </tr>
+    '<% _.forEach(posts, function(post) { %> ' +
+    '<tr><td><%- post.topic %></td>' +
+    '<td><%- post.replies.length %></td>' +
+    '</tr><% }); %>'
 );
+
+var renderForum = function(data) {
+    $('#forumBody').html(postRowTemplate(data));
+};
+
+var hideAndClear = function() {
+    $('#newTopicForm').hide();
+    $('#addNewTopic').show();
+    $('#topic').val('');
+    $('#message').val('');
+};
+
+
