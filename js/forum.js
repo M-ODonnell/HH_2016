28928b38823767672a80102f9eb10ccc1f606338
Meghan O'Donnell <meghan.tara@gmail.com>
2016-05-14 17:45:26 -0400

    trying to see single post

--- js/forum.js
+++ js/forum.js
@@ -22,21 +22,35 @@ $(document).ready(function() {
         renderForum(data);
         hideAndClear();
     });
-
-
+    $('#forumBody').click('a.expandPost', function(event) {
+        event.preventDefault();
+        console.log("EVENT: ", event);
+        console.log('single post link clicked');
+        var postID = $(event).data('post');
+        renderPost(data.posts[postID]);
+    });
 
 });
 
-var postRowTemplate = _.template(
-    '<% _.forEach(posts, function(post) { %> ' +
-    '<tr><td><%- post.topic %></td>' +
-    '<td><%- post.replies.length %></td>' +
-    '</tr><% }); %>'
+var forumTemplate = _.template(
+    '<table class="bordered highlight"><thead><tr><th>Topic</th><th>Replies</th></tr></thead><tbody>' +
+    '<% _.forEach(posts, function(post, index) { %> ' +
+    '   <tr><td><a href="#" class="expandPost" data-post="<%- index %>"><%- post.topic %></a></td>' +
+    '   <td><%- post.replies.length %></td>' +
+    '   </tr><% }); %>' +
+    '</tbody></table>'
 );
 
+var postTemplate = _.template('<h3><%- post.topic %></h3><p><%- post.user %></p><p><%- post.message %></p>');
+
 var renderForum = function(data) {
-    $('#forumBody').html(postRowTemplate(data));
+    $('#forumBody').html(forumTemplate(data));
 };
+
+var renderPost = function(data) {
+    $('#forumBody').html(postTemplate(data));
+};
+
 
 var hideAndClear = function() {
     $('#newTopicForm').hide();
