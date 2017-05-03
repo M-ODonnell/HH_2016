0c9f22b0e41e871d3d2cbcb1f014cf7f78a0170f
Meghan O'Donnell <M-ODonnell@users.noreply.github.com>
2017-05-03 16:36:16 -0400

    Update connectvars.php

--- db/connectvars.php
+++ db/connectvars.php
@@ -2,7 +2,7 @@
 
 	// Define the database connection constants here
 	define('DB_HOST','http://php.scweb.ca/');
-	define('DB_USER','W0665618');
-	define('DB_PASSWORD','ntmdntmd');
+	define('DB_USER','');
+	define('DB_PASSWORD','');
 	define('DB_NAME','HH2016');
 ?>
